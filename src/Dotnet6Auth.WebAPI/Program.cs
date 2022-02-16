b61de8cb395b09bbc298a4774dce9e5f6d9e44de
bmassioui <bouchaib.massioui@gmail.com>
2022-02-16 10:27:59 +0000

    controller

--- src/Dotnet6Auth.WebAPI/Program.cs
+++ src/Dotnet6Auth.WebAPI/Program.cs
@@ -1,6 +1,29 @@
+using Dotnet6Auth.WebAPI.Authorization;
+using Dotnet6Auth.WebAPI.Helpers;
+using Dotnet6Auth.WebAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
+
+// use sql server db in production and sqlite db in development
+if (env.IsProduction())
+    builder.Services.AddDbContext<DataContext>();
+else
+    builder.Services.AddDbContext<DataContext, SqliteDataContext>();
 
 // Add services to the container.
+builder.Services.AddCors();
+
+// configure automapper with all automapper profiles from this assembly
+builder.Services.AddAutoMapper(typeof(Program));
+
+// configure strongly typed settings object
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+// configure DI for application services
+builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +31,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// migrate any database changes on startup (includes initial db creation)
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dataContext.Database.Migrate();
+}
+
+// configure HTTP request pipeline
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+// global error handler
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
+// custom jwt auth middleware
+app.UseMiddleware<JwtMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
